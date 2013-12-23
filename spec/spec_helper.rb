00287b4a0e80ab1d5179ac28fce391e55e885575
mmwtsn <mwatson@dosomething.org>
2013-12-23 11:27:12 -0500

    Removes VCR gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'rack/test'
-require 'webmock'
-require 'vcr'
+require 'webmock/rspec'
 require_relative '../app.rb'
 
 set :environment, :test
@@ -14,9 +13,12 @@ end
 RSpec.configure do |c|
   c.include Rack::Test::Methods
   c.include RSpecHelper
+  c.before(:each) do
+    stub_request(:get, "https://api.hipchat.com/v2/emoticon?auth_token=#{ENV['HIPCHAT_API']}").
+    with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'api.hipchat.com', 'User-Agent'=>'Ruby'}).
+    to_return(:status => 200, :body => "stubbed response", :headers => {})
+  end
 end
 
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/fixtures/dish_cassettes'
-  c.hook_into :webmock
-end
+# Prevent test suite from making external HTTP requests
+WebMock.disable_net_connect!(allow_localhost: true)
