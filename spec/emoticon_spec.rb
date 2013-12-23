2883bbe17b22fc7b4a536bc7d101602be7ecb5c8
mmwtsn <mwatson@dosomething.org>
2013-12-23 11:27:39 -0500

    Removes VCR gem

--- spec/emoticon_spec.rb
+++ spec/emoticon_spec.rb
@@ -1,7 +1,6 @@
 require_relative './spec_helper'
 
-describe Helpers::HipChat do
-
+describe 'Helpers::HipChat' do
   describe 'attributes' do
     it 'includes HTTParty' do
       Helpers::HipChat.should include(HTTParty)
@@ -12,19 +11,26 @@ describe Helpers::HipChat do
     end
   end
 
-  describe 'GET emoticon' do
-    let(:hipchat) { Helpers::HipChat.new }
-
-    before do
-      VCR.insert_cassette 'emoticon', :record => :new_episodes
-    end
-   
-    after do
-      VCR.eject_cassette
-    end
-
-    it 'when requested' do
+  describe '.get_emoticons' do
+    it 'makes a successful GET request' do
+      hipchat = Helpers::HipChat.new
       hipchat.get_emoticons.response.code.should eql "200"
     end
+  end
+end
+
+describe 'Helpers.all_emoticons' do
+  it 'returns a hash' do
+    all_emoticons.class.should eql String
+  end
+end
+
+describe 'Emoticon external request' do
+  it 'queries the HipChat API' do
+    uri = URI("https://api.hipchat.com/v2#{ENV['HIPCHAT_API']}")
+
+    response = Net::HTTP.get(uri)
+
+    expect(response).to be_an_instance_of(String)
   end
 end
