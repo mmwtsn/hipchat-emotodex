ae3302d48498bf6b8836900acbffd87acb72ee81
mmwtsn <mwatson@dosomething.org>
2013-12-26 15:03:33 -0500

    Simplifies application spec

--- spec/emoticon_spec.rb
+++ spec/emoticon_spec.rb
@@ -1,13 +1,11 @@
 require_relative './spec_helper'
 
-describe 'Helpers::HipChat' do
-  describe 'attributes' do
-    it 'includes HTTParty' do
-      Helpers::HipChat.should include(HTTParty)
-    end
+describe HipChat do
+  it 'includes HTTParty' do
+    HipChat.should include(HTTParty)
+  end
 
-    it 'calls the correct endpoint' do
-      Helpers::HipChat.base_uri.should eql 'https://api.hipchat.com/v2'
-    end
+  it 'calls the correct endpoint' do
+    HipChat.base_uri.should eql 'https://api.hipchat.com/v2'
   end
 end
