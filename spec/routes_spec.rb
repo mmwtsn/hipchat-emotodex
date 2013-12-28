2ad0af5babb4e851668bbae594dfa62811991545
M. Maxwell Watson <hi@mmwtsn.com>
2013-12-28 17:10:19 -0500

    Updated Sinatra spec

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -3,11 +3,16 @@ require_relative './spec_helper'
 describe 'This Sinatra application' do
   it 'should load the home page' do
     get '/'
-    last_response.status.should == 200
+    last_response.status.should eql 200
+  end
+
+  it 'should not error out' do
+    get '/'
+    last_response.status.should_not eql 500
   end
 
   it 'should not load undefined routes' do
     get '/sinatradoesntknowthisditty'
-    last_response.status.should == 404
+    last_response.status.should eql 404
   end
 end
